40aa3f680c2c6e852095efb2ba9aa223b6c7e562
loujine <loujine@protonmail.com>
2021-04-05 20:33:46 +0000

    wip

--- mb-reledit-set_rec_artist_as_writer.user.js
+++ mb-reledit-set_rec_artist_as_writer.user.js
@@ -1,10 +1,10 @@
-/* global $ MB relEditor */
+/* global $ MB relEditor requests server */
 'use strict';
 // ==UserScript==
 // @name         MusicBrainz relation editor: Set writer relation from recording artist
 // @namespace    mbz-loujine
 // @author       loujine
-// @version      2021.4.4
+// @version      2021.4.5
 // @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/set_writer/mb-reledit-set_rec_artist_as_writer.user.js
 // @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/set_writer/mb-reledit-set_rec_artist_as_writer.user.js
 // @supportURL   https://github.com/loujine/musicbrainz-scripts
@@ -18,28 +18,33 @@
 // @run-at       document-end
 // ==/UserScript==
 
-function setWriter() {
-    const selectedRecordings = MB.relationshipEditor.UI.checkedRecordings();
+function setWriter(workRels, artistCreditNames) {
     const vm = MB.releaseRelationshipEditor;
-
-    selectedRecordings.map(rec => {
-        rec.artistCredit.names.map(credit => {
-            rec.relationships().filter(
-                rel => rel.entityTypes === 'recording-work'
-            ).map(rel => {
-                const work = rel.entities()[1];
-                console.log(work);
-                const dialog = new MB.relationshipEditor.UI.AddDialog({
-                    viewModel: vm,
-                    source: work,
-                    target: credit.artist,
-                });
-                dialog.accept();
+    artistCreditNames.map(credit => {
+        workRels.map(rel => {
+            const work = rel.entities()[1];
+            const dialog = new MB.relationshipEditor.UI.AddDialog({
+                viewModel: vm,
+                source: work,
+                target: credit.artist,
             });
+            dialog.accept();
         });
     });
 }
 
+function applyWriter() {
+    let idx = 0;
+
+    MB.relationshipEditor.UI.checkedRecordings().forEach(rec => {
+        setTimeout(function () {
+            requests.GET(`/ws/js/entity/${rec.gid}`, resp => {
+                setWriter(rec.performances(), JSON.parse(resp).artistCredit.names);
+            });
+        }, idx * server.timeout);
+        idx += 1;
+    });
+}
 
 (function displayToolbar() {
     relEditor.container(document.querySelector('div.tabs'))
@@ -58,7 +63,7 @@ function setWriter() {
 $(document).ready(function () {
     let appliedNote = false;
     document.getElementById('setWriter').addEventListener('click', () => {
-        setWriter();
+        applyWriter();
         if (!appliedNote) {
             relEditor.editNote(GM_info.script);
             appliedNote = true;
